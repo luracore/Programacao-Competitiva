0cebd5b555a0adcb7046990ff5dbab78b56f41c3
Lucca Rafael Costa Resende <1411826@PLU4PLFLL130108.pucminas.net>
2026-04-16 12:07:23 -0300

    happyNumber

---
+++ happyNumber.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+// Questão: https://atcoder.jp/contests/abc439/tasks/abc439_b
+
+int rec(int n){
+  if(n == 0) return 0;
+  int resto = n/10;
+  int atual = n - (resto * 10);
+  return pow(atual, 2) + rec(resto);
+}
+
+int isHappyNumber(int n){
+  while(n > 10){
+    n = rec(n);
+  }
+  return n == 10 || n == 7 || n == 1;
+}
+
+int main(){
+  
+  int n;
+  scanf("%d", &n);
+
+  printf("%s\n", isHappyNumber(n) ? "Yes" : "No");
+
+  return 0;
+}
